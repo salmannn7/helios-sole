dab2ad7de97d8148db9ef16d918a3b66695a8a59
Salman-Talib7 <21309782@stu.mmu.ac.uk>
2024-12-24 16:03:38 +0000

    added base

--- frontend-app/vite.config.js
+++ frontend-app/vite.config.js
@@ -1,11 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/helios-sole/', // Ensure the base path matches your repository name
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
